c567ab3e33f2dc69cc4e17e63b1e5a5576e76e88
tianguau <xianzw@163.com>
2020-01-26 12:19:55 +0800

    add ie types

--- scapy/contrib/pfcp.py
+++ scapy/contrib/pfcp.py
@@ -25,13 +25,13 @@ Protocol as defined in 3GPP TS 29.244.
 from scapy.packet import bind_layers, bind_bottom_up, Packet
 from scapy.fields import BitField, ByteEnumField, ShortField, \
     ConditionalField, LongField, ThreeBytesField, ByteField, \
-    MultipleTypeField, PacketListField
+    MultipleTypeField, PacketListField, ShortEnumField
 from scapy.layers.inet import UDP
 
 PFCPMsgType = {
     #0 Reserved
 
-	#PFCP Node related messages
+    #PFCP Node related messages
     1  : "PFCP Heartbeat Request"
     2  : "PFCP Heartbeat Response"
     3  : "PFCP PFD Management Request"
@@ -49,7 +49,7 @@ PFCPMsgType = {
     15 : "PFCP Session Set Deletion Response"
     #16 to 49 For future use
 
-	#PFCP Session related messages
+    #PFCP Session related messages
     50 : "PFCP Session Establishment Request"
     51 : "PFCP Session Establishment Response"
     52 : "PFCP Session Modification Request"
@@ -59,7 +59,7 @@ PFCPMsgType = {
     56 : "PFCP Session Report Request"
     57 : "PFCP Session Report Response"
     #58 to 99 For future use
-	#Other messages
+    #Other messages
     #100 to 255 For future use
 }
 
@@ -81,18 +81,180 @@ class PFCP(Packet) :
             length_from=lambda pkt:pkt.Length - (16 if pkt.S==1 else 8))
     ]
 
+IE_Types = {
+    0   : "Reserved",
+    1   : "Create PDR",
+    2   : "PDI",
+    3   : "Create FAR",
+    4   : "Forwarding Parameters",
+    5   : "Duplicating Parameters",
+    6   : "Create URR",
+    7   : "Create QER",
+    8   : "Created PDR",
+    9   : "Update PDR",
+    10  : "Update FAR",
+    11  : "Update Forwarding Parameters",
+    12  : "Update BAR (PFCP Session Report Response)",
+    13  : "Update URR",
+    14  : "Update QER",
+    15  : "Remove PDR",
+    16  : "Remove FAR",
+    17  : "Remove URR",
+    18  : "Remove QER",
+    19  : "Cause",
+    20  : "Source Interface",
+    21  : "F-TEID",
+    22  : "Network Instance",
+    23  : "SDF Filter",
+    24  : "Application ID",
+    25  : "Gate Status",
+    26  : "MBR",
+    27  : "GBR",
+    28  : "QER Correlation ID",
+    29  : "Precedence",
+    30  : "Transport Level Marking",
+    31  : "Volume Threshold",
+    32  : "Time Threshold",
+    33  : "Monitoring Time",
+    34  : "Subsequent Volume Threshold",
+    35  : "Subsequent Time Threshold",
+    36  : "Inactivity Detection Time",
+    37  : "Reporting Triggers",
+    38  : "Redirect Information",
+    39  : "Report Type",
+    40  : "Offending IE",
+    41  : "Forwarding Policy",
+    42  : "Destination Interface",
+    43  : "UP Function Features",
+    44  : "Apply Action",
+    45  : "Downlink Data Service Information",
+    46  : "Downlink Data Notification Delay",
+    47  : "DL Buffering Duration",
+    48  : "DL Buffering Suggested Packet Count",
+    49  : "PFCPSMReq-Flags",
+    50  : "PFCPSRRsp-Flags",
+    51  : "Load Control Information",
+    52  : "Sequence Number",
+    53  : "Metric",
+    54  : "Overload Control Information",
+    55  : "Timer",
+    56  : "Packet Detection Rule ID",
+    57  : "F-SEID",
+    58  : "Application ID's PFDs",
+    59  : "PFD context",
+    60  : "Node ID",
+    61  : "PFD contents",
+    62  : "Measurement Method",
+    63  : "Usage Report Trigger",
+    64  : "Measurement Period",
+    65  : "FQ-CSID",
+    66  : "Volume Measurement",
+    67  : "Duration Measurement",
+    68  : "Application Detection Information",
+    69  : "Time of First Packet",
+    70  : "Time of Last Packet",
+    71  : "Quota Holding Time",
+    72  : "Dropped DL Traffic Threshold",
+    73  : "Volume Quota",
+    74  : "Time Quota",
+    75  : "Start Time",
+    76  : "End Time",
+    77  : "Query URR",
+    78  : "Usage Report (in Session Modification Response)",
+    79  : "Usage Report (Session Deletion Response)",
+    80  : "Usage Report (Session Report Request)",
+    81  : "URR ID",
+    82  : "Linked URR ID",
+    83  : "Downlink Data Report",
+    84  : "Outer Header Creation",
+    85  : "Create BAR",
+    86  : "Update BAR (Session Modification Request)",
+    87  : "Remove BAR",
+    88  : "BAR ID",
+    89  : "CP Function Features",
+    90  : "Usage Information",
+    91  : "Application Instance ID",
+    92  : "Flow Information",
+    93  : "UE IP Address",
+    94  : "Packet Rate",
+    95  : "Outer Header Removal",
+    96  : "Recovery Time Stamp",
+    97  : "DL Flow Level Marking",
+    98  : "Header Enrichment",
+    99  : "Error Indication Report",
+    100 : "Measurement Information",
+    101 : "Node Report Type",
+    102 : "User Plane Path Failure Report",
+    103 : "Remote GTP-U Peer",
+    104 : "UR-SEQN",
+    105 : "Update Duplicating Parameters",
+    106 : "Activate Predefined Rules",
+    107 : "Deactivate Predefined Rules",
+    108 : "FAR ID",
+    109 : "QER ID",
+    110 : "OCI Flags",
+    111 : "PFCP Association Release Request",
+    112 : "Graceful Release Period",
+    113 : "PDN Type",
+    114 : "Failed Rule ID",
+    115 : "Time Quota Mechanism",
+    116 : "User Plane IP Resource Information",
+    117 : "User Plane Inactivity Timer",
+    118 : "Aggregated URRs",
+    119 : "Multiplier",
+    120 : "Aggregated URR ID",
+    121 : "Subsequent Volume Quota",
+    122 : "Subsequent Time Quota",
+    123 : "RQI",
+    124 : "QFI",
+    125 : "Query URR Reference",
+    126 : "Additional Usage Reports Information",
+    127 : "Create Traffic Endpoint",
+    128 : "Created Traffic Endpoint",
+    129 : "Update Traffic Endpoint",
+    130 : "Remove Traffic Endpoint",
+    131 : "Traffic Endpoint ID",
+    132 : "Ethernet Packet Filter",
+    133 : "MAC address",
+    134 : "C-TAG",
+    135 : "S-TAG",
+    136 : "Ethertype",
+    137 : "Proxying",
+    138 : "Ethernet Filter ID",
+    139 : "Ethernet Filter Properties",
+    140 : "Suggested Buffering Packets Count",
+    141 : "User ID",
+    142 : "Ethernet PDU Session Information",
+    143 : "Ethernet Traffic Information",
+    144 : "MAC Addresses Detected",
+    145 : "MAC Addresses Removed",
+    146 : "Ethernet Inactivity Timer",
+    147 : "Additional Monitoring Time",
+    148 : "Event Quota",
+    149 : "Event Threshold",
+    150 : "Subsequent Event Quota",
+    151 : "Subsequent Event Threshold",
+    152 : "Trace Information",
+    153 : "Framed-Route",
+    154 : "Framed-Routing",
+    155 : "Framed-IPv6-Route",
+    156 : "Event Time Stamp",
+    157 : "Averaging Window",
+    158 : "Paging Policy Indicator"
+}
+
+class IE_NotImplementedTLV(Packet):
+    name = "IE not implemented"
+    fields_desc = [ShortEnumField("ietype", 0, IEType),
+                   ShortField("length", None),
+                   StrLenField("data", "", length_from=lambda x: x.length)]
+
 def IE_Dispatcher(s):
     """Choose the correct Information Element class."""
-
     # Get the IE type
-    ietype = orb(s[0])
-    cls = ietypecls.get(ietype, Raw)
-
-    # if ietype greater than 128 are TLVs
-    if cls is Raw and ietype > 128:
-        cls = IE_NotImplementedTLV
-
-    return cls(s)
+    ietype = orb(s[0])*16 + orb(s[1])
+    cls = ietypecls.get(ietype, IE_NotImplementedTLV)
+    return cls[0](s)
 
 class PFCPMessaage(Packet):
     """PFPCP Messages"""
@@ -107,94 +269,96 @@ class _PFCPMsgSession(PFCPMessaage):
 
 class PFCPMsgNodeHeartBeatReq(_PFCPMsgNode):
     """ 1 : PFCP Heartbeat Request """
-    pass
+    name = "PFCP Heartbeat Request"
 
 class PFCPMsgNodeHeartBeatResp(_PFCPMsgNode):
     """ 2 : PFCP Heartbeat Response """
-    pass
+    name = "PFCP Heartbeat Response"
 
 class PFCPMsgNodePfdManagmentReq(_PFCPMsgNode):
     """ 3 : PFCP PFD Management Request """
-    pass
+    name = "PFCP PFD Management Request"
 
 class PFCPMsgNodePfdManagmentResp(_PFCPMsgNode):
     """ 4 : PFCP PFD Management Response """
-    pass
+    name = "PFCP PFD Management Response"
+
 
 class PFCPMsgNodeAssociationSetupReq(_PFCPMsgNode):
     """ 5 : PFCP Association Setup Request """
-    pass
+    name = "PFCP Association Setup Request"
 
 class PFCPMsgNodeAssociationSetupResp(_PFCPMsgNode):
     """ 6 : PFCP Association Setup Response """
-    pass
+    name = "PFCP Association Setup Response"
 
 class PFCPMsgNodeAssociationUpdateReq(_PFCPMsgNode):
     """ 7 : PFCP Association Update Request """
-    pass
+    name = "PFCP Association Update Request"
 
 class PFCPMsgNodeAssociationUpdateResp(_PFCPMsgNode):
     """ 8 : PFCP Association Update Response """
-    pass
+    name = "PFCP Association Update Response"
 
 class PFCPMsgNodeAssociationReleaseReq(_PFCPMsgNode):
     """ 9 : PFCP Association Release Request """
-    pass
+    name = "PFCP Association Release Request"
 
 class PFCPMsgNodeAssociationReleaseResp(_PFCPMsgNode):
     """ 10 : PFCP Association Release Response """
-    pass
+    name = "PFCP Association Release Response"
 
 class PFCPMsgNodeVersionNotSupportResp(_PFCPMsgNode):
     """ 11 : PFCP Version Not Supported Response """
-    pass
+    name = "PFCP Version Not Supported Response"
 
 class PFCPMsgNodeNodeReportReq(_PFCPMsgNode):
     """ 12 : PFCP Node Report Request """
-    pass
+    name = "PFCP Node Report Request"
 
 class PFCPMsgNodeNodeReportResp(_PFCPMsgNode):
     """ 13 : PFCP Node Report Response """
-    pass
+    name = "PFCP Node Report Response"
 
 class PFCPMsgNodeSessionSetDeleteReq(_PFCPMsgNode):
     """ 14 : PFCP Session Set Deletion Request """
-    pass
+    name = "PFCP Session Set Deletion Request"
 
 class PFCPMsgNodeSessionSetDeleteResp(_PFCPMsgNode):
     """ 15 : PFCP Session Set Deletion Response """
-    pass
+    name = "PFCP Session Set Deletion Response"
 
 class PFCPMsgSessionEstablishReq(_PFCPMsgSession):
     """ 50 : PFCP Session Establishment Request """
-    pass
+    name = "PFCP Session Establishment Request"
 
 class PFCPMsgSessionEstablishResp(_PFCPMsgSession):
     """ 51 : PFCP Session Establishment Response """
-    pass
+    name = "PFCP Session Establishment Response"
 
 class PFCPMsgSessionModificationReq(_PFCPMsgSession):
     """ 52 : PFCP Session Modification Request :"""
-    pass
+    name = "PFCP Session Modification Request"
 
 class PFCPMsgSessionModificationResp(_PFCPMsgSession):
     """ 53 : PFCP Session Modification Response """
-    pass
+    name = "PFCP Session Modification Response"
 
 class PFCPMsgSessionDeletionReq(_PFCPMsgSession):
     """ 54 : PFCP Session Deletion Request """
+    name = "PFCP Session Deletion Request"
 
 class PFCPMsgSessionDeletionResp(_PFCPMsgSession):
     """ 55 : PFCP Session Deletion Response """
-    pass
+    name = "PFCP Session Deletion Response"
 
 class PFCPMsgSessionReportReq(_PFCPMsgSession):
-    """ 56	PFCP Session Report Request """
-    pass
+    """ 56 : PFCP Session Report Request """
+    name = "PFCP Session Report Request"
 
 class PFCPMsgSessionReportReq(_PFCPMsgSession):
     """ 57 : PFCP Session Report Response """
-    pass
+    name = "PFCP Session Report Response"
 
 
 # Bind GTP-C
